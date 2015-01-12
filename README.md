2b5c63c7ccd5c505e842755ed099347667a71d77
José María Martínez Otzeta <jmartinezot@gmail.com>
2015-01-12 19:10:00 +0100

    README.md modified. Added command line options to the MatrixToGraph utility. Added examples directory.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 MatrixToGraph
 =============
 
-Creating a graph from a matrix
+The utility MatrixToGraph creates a graph from an adjacency matrix with weights associated.
+
+Maybe you need to install cmake, boost and/or gnuplot. If so, type
+
+**sudo apt-get install cmake libboost-all-dev gnuplot**
+
+To compile the utility in a Ubuntu system, just go to the MatrixToGraph directory and type
+
+**cmake .**  
+**make**
+
+The usage is 
+
+**./MatrixToGraph --CSV_filename=filename.csv --Graphviz_filename=filename.graph**
+
+Once the file **filename.graph** has been generated, several graph types can been created.
+
+- *Dot graph*: **dot -Tpdf filename.graph -o filename.pdf**
+- *Circo graph*: **circo -Tpdf filename.graph -o filename.pdf**
+- *Neato graph*: **neato -Tpdf filename.graph -o filename.pdf**
+- *Twopi graph*: **twopi -Tpdf filename.graph -o filename.pdf**
+
+The above commands create graphs in PDF format. Other formats are possible. For example, **dot -Tps filename.graph -o filename.ps** would create a graph in PS format.
+
+
+
+
--- src/MatrixToGraph.cpp
+++ src/MatrixToGraph.cpp
@@ -1,4 +1,5 @@
 #include <Utils.hpp>
+#include <boost/program_options.hpp>
 
 /** @mainpage MatrixToGraph
  * 
@@ -6,16 +7,65 @@
  * 
 */
 
+using namespace std;
+namespace po = boost::program_options;
+
 int main (int argc, char** argv)
 {
 	AttributeMatrix attributeMatrix;
 	AdjacencyMatrix adjacencyMatrix;
 	
+	std::string CSV_filename, Graphviz_filename;
+	
+   try {
+	  // Parses the command line options.
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("CSV_filename", po::value<std::string>(), "set the name of the CSV file which contains the matrix")
+            ("Graphviz_filename", po::value<std::string>(), "set the name of the file containing the graph")
+        ;
+
+        po::variables_map vm;        
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);    
+
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return 0;
+        }
+        
+        if (vm.count("CSV_filename"))
+			CSV_filename = vm["CSV_filename"].as<std::string>();
+		else
+			{
+				cout << "CSV file name missing" << endl;
+				cout << desc << endl;
+				return 0;
+			}    
+        if (vm.count("Graphviz_filename"))
+			Graphviz_filename = vm["Graphviz_filename"].as<std::string>();
+		else
+			{
+				cout << "Graphviz filename missing" << endl;
+				cout << desc << endl;
+				return 0;
+			}
+    }
+    catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+
+	
 	// read the matrix from a CSV file
-	adjacencyMatrix = Utils::CSVToAdjacencyMatrix(argv[1]);
+	adjacencyMatrix = Utils::CSVToAdjacencyMatrix(CSV_filename);
 
 	// write AdjacencyMatrix to Graphviz format
-	Utils::AdjacencyMatrixToGraphviz(adjacencyMatrix, argv[2]);
+	Utils::AdjacencyMatrixToGraphviz(adjacencyMatrix, Graphviz_filename);
 
 	return 0;
 }
